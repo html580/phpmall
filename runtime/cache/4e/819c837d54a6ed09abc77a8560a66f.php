8e33e0b1bd93a7318272e18dff16a4b67de6205d
html580 <html580@126.com>
2017-06-02 13:05:35 +0800

    删除文件

--- runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000000a:4:{s:10:"pageFooter";a:1:{i:0;s:27:"\addons\returntop\Returntop";}s:19:"documentDetailAfter";a:1:{i:1;s:17:"\addons\digg\Digg";}s:16:"adminArticleEdit";a:1:{i:0;s:37:"\addons\editorforadmin\Editorforadmin";}s:10:"AdminIndex";a:3:{i:0;s:25:"\addons\sitestat\Sitestat";i:1;s:29:"\addons\systeminfo\Systeminfo";i:2;s:23:"\addons\devteam\Devteam";}}
-?>
